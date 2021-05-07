414d8922aa802b84b1c88491bac99561b27424c4
Muntakim Rahman <muntakim.rahman@gmail.com>
2021-05-07 14:19:56 -0400

    Working Database Structure

--- app.py
+++ app.py
@@ -1,10 +1,201 @@
-import database 
+import tkinter as tk
+from tkinter.font import Font
+from tkinter import messagebox
 
-database.create_table()
+import database as db
 
-database.add_record(
-    ['Muntakim', 'Mahir Tanzil', 'Merina', 'Tunzilur'],
-    ['Rahman','Rahman','Sultana', 'Rahman'],
-    ['muntakim.rahman@gmail.com', 'mahir199.tanzil@gmail.com', 'merina_70@yahoo.com', 'tunzilur@gmail.com'])
+# Create Tk Window.
+customers_window = tk.Tk()
+customers_window.title('Customers Information')
+customers_window.iconbitmap('Images\Icons\Database_Icon.ico')
 
-database.show_records_all()
+# Initialize customization variables for GUI Widgets.
+button_bg = '#0D4BC5'
+button_font = Font(family = 'Arial', size = 12, weight = 'bold')
+
+general_label_bg = '#09F90D'
+general_label_font = Font(family = 'Ubuntu Mono', size = 10, weight = 'bold')
+
+general_entry_bg = '#0DC5B5'
+general_entry_font = Font(family = 'Ubuntu Mono', size = 10, weight = 'normal')
+
+##############################################
+############## New Customers #################
+##############################################
+
+# Create a Frame for Widgets regarding New Customers.
+new_customers_frame = tk.Frame(customers_window)
+# Position Frame onto Screen.
+new_customers_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+# Create the Table to Contain Customer Information.
+db.create_table_customers()
+
+# Create Entry Field Labels for New Customers.
+new_customers_label = tk.Label(new_customers_frame, text = 'New Customer Information',
+    bg = general_label_bg, fg = 'Black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+new_customers_label.grid(row = 0, column = 0, padx = 5, pady = (0, 5))
+
+first_name_label = tk.Label(new_customers_frame, text = 'First Name : ', 
+    bg = general_label_bg, fg = 'Black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+first_name_label.grid(row = 1, column = 0, padx = 5, pady = 5)
+
+last_name_label = tk.Label(new_customers_frame, text = 'Last Name : ',
+    bg = general_label_bg, fg = 'Black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+last_name_label.grid(row = 2, column = 0, padx = 5, pady = 5)
+
+email_address_label = tk.Label(new_customers_frame, text = 'Email Address : ', 
+    bg = general_label_bg, fg = 'Black', font = general_label_font, borderwidth = 2,  anchor = tk.E)
+email_address_label.grid(row = 3, column = 0, padx = 5, pady = 5)
+
+# Create Entry Widgets for New Customers.
+first_name = tk.Entry(new_customers_frame, width = 30, bg = general_entry_bg, fg = 'black', 
+    font = general_entry_font, borderwidth = 2)
+first_name.grid(row = 1, column = 1, padx = (0, 5), pady = 5)
+
+last_name = tk.Entry(new_customers_frame, width = 30, bg = general_entry_bg, fg = 'black', 
+    font = general_entry_font, borderwidth = 2)
+last_name.grid(row = 2, column = 1, padx = (0, 5), pady = 5)
+
+email_address = tk.Entry(new_customers_frame, width = 30, bg = general_entry_bg, fg = 'black', 
+    font = general_entry_font, borderwidth = 2)
+email_address.grid(row = 3, column = 1, padx = (0, 5), pady = 5)
+
+# Insert Default Text to Entry Widgets.
+first_name.insert(0, 'First Name')
+last_name.insert(0, 'Last Name')
+email_address.insert(0, 'Email Address')
+
+def add_customer () :
+    new_customer = [first_name.get(), last_name.get(), email_address.get()]
+    db.add_records_customers(str(new_customer[0]), str(new_customer[1]), str(new_customer[2]))
+
+# Create Add Customer Button.
+addCustomerButton = tk.Button(new_customers_frame, text = 'Add Customer', bg = button_bg, 
+    fg = 'white', font = button_font, borderwidth = 2, command = add_customer)
+addCustomerButton.grid(row = 4, column = 2, padx = 10, pady = 10)
+
+############################################################
+################ Access and Delete Records ##################
+############################################################
+
+# Create a Frame for Access and Delete Record Functions.
+query_delete_frame = tk.Frame(customers_window)
+# Position Frame onto Screen.
+query_delete_frame.grid(row = 1, column = 0, padx = 10, pady = 10)
+
+def show_customer_records () :
+    column_to_organize = str(order_information.get())
+    customer_records = db.show_records_all_customers(column_to_organize)
+
+    # Create a Pop Up Informatic for the Customer Records.
+    messagebox.showinfo('Customer Records', str(customer_records))
+
+# Create Label for Order Information.
+order_information_label = tk.Label(query_delete_frame, text = 'Order By : ', bg = general_label_bg, 
+    fg = 'Black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+order_information_label.grid(row = 0, column = 0, padx = 5, pady = (0, 5))
+
+# Create Entry Widget for Order Information.
+order_information = tk.Entry(query_delete_frame, width = 30, bg = general_entry_bg, fg = 'black', 
+    font = general_entry_font, borderwidth = 2)
+order_information.grid(row = 0, column = 1, padx = (0, 5), pady = 5)
+order_information.insert(0, 'Column Name')
+
+# Create Query Button.
+queryButton = tk.Button(query_delete_frame, text = 'Query', bg = button_bg, fg = 'white', 
+    font = button_font, borderwidth = 2, command = show_customer_records)
+queryButton.grid(row = 0, column = 2, padx = 5, pady = 5)
+
+# Create Label for ID.
+id_box_label = tk.Label(query_delete_frame, text = 'Key ID : ', bg = general_label_bg, fg = 'black', 
+    font = general_label_font, borderwidth = 2, anchor = tk.E)
+id_box_label.grid(row = 1, column = 0, padx = 5, pady = 5)
+
+# Create Entry Widget for ID.
+id_box = tk.Entry(query_delete_frame, width = 30, bg = general_entry_bg, fg = 'black', 
+    font = general_entry_font, borderwidth = 2)
+id_box.grid(row = 1, column = 1, padx = (0, 5), pady = 5)
+id_box.insert(0, 'Enter Key ID to Change/Delete')
+
+def delete_customer() :
+    id_to_delete = id_box.get()
+    db.delete_record_customer(id_to_delete)
+
+# Create Delete Record Button.
+deleteRecordButton = tk.Button(query_delete_frame, text = 'Delete Customer', bg = button_bg, fg = 'white', 
+    font = button_font, borderwidth = 2, command = delete_customer)
+deleteRecordButton.grid(row = 2, column = 0, padx = 10, pady = 10)
+
+def open_customer_edit_window() :
+    # Create Tk Window.
+    global edit_window
+    edit_window = tk.Tk()
+    edit_window.title('New Information')
+    edit_window.iconbitmap('Images/Icons/Database_Icon.ico')
+
+    id_to_change = id_box.get()
+
+    customer_to_modify = db.show_record_specific_customer(id_to_change)
+
+    print(customer_to_modify)
+
+    # Create Entry Field Labels for New Customer.
+    edit_customer_label = tk.Label(edit_window, text = 'New Customer Information', 
+        bg = general_label_bg, fg = 'black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+    edit_customer_label.grid(row = 0, column = 0, padx = 5, pady = 5)
+
+    edit_first_name_label = tk.Label(edit_window, text = 'First Name : ',
+        bg = general_label_bg, fg = 'black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+    edit_first_name_label.grid(row = 1, column = 0, padx = 5, pady = 5)
+
+    edit_last_name_label = tk.Label(edit_window, text = 'Last Name : ',         
+        bg = general_label_bg, fg = 'black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+    edit_last_name_label.grid(row = 2, column = 0, padx = 5, pady = 5)
+
+    edit_email_address_label = tk.Label(edit_window, text = 'Email Address : ', 
+        bg = general_label_bg, fg = 'black', font = general_label_font, borderwidth = 2, anchor = tk.E)
+    edit_email_address_label.grid(row = 3, column = 0, padx = 5, pady = 5)
+
+    # Create Entry Widgets for New Customers.
+    global edit_first_name
+    edit_first_name = tk.Entry(edit_window, width = 30, bg = general_entry_bg, 
+        fg = 'black', font = general_entry_font, borderwidth = 2)
+    edit_first_name.grid(row = 1, column = 1, padx = (0, 5), pady = 5)
+    edit_first_name.insert(0, customer_to_modify[0])
+
+    global edit_last_name
+    edit_last_name = tk.Entry(edit_window, width = 30, bg = general_entry_bg, 
+        fg = 'black', font = general_entry_font, borderwidth = 2)
+    edit_last_name.grid(row = 2, column = 1, padx = (0, 5), pady = 5)
+    edit_last_name.insert(0, customer_to_modify[1])
+
+    global edit_email_address
+    edit_email_address = tk.Entry(edit_window, width = 30, bg = general_entry_bg, 
+        fg = 'black', font = general_entry_font, borderwidth = 2)
+    edit_email_address.grid(row = 3, column = 1, padx = (0, 5), pady = 5)
+    edit_email_address.insert(0, customer_to_modify[2])
+
+    # Create Submit Button.
+    submitButton = tk.Button(edit_window, text = 'Submit Information',
+        bg = button_bg, fg = 'white', font = button_font, borderwidth = 2, command = modify_customer)
+    submitButton.grid(row = 4, column = 2, padx = 5, pady = 5)
+
+def modify_customer () :
+    id_to_change = id_box.get()
+    modified_first_name = edit_first_name.get()
+    modified_last_name = edit_last_name.get()
+    modified_email_address = edit_email_address.get()
+    db.change_record_specific_customer(id_to_change, modified_first_name, 
+        modified_last_name, modified_email_address)
+
+    # Close the Edit Window.
+    edit_window.destroy()
+
+# Create Modify Record Button.
+modifyRecordButton = tk.Button(query_delete_frame, text = 'Change Information', 
+    bg = button_bg, fg = 'white', font = button_font, borderwidth = 2, command = open_customer_edit_window)
+modifyRecordButton.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+# Infinite Loop -> Interrupted by Keyboard or Mouse.
+customers_window.mainloop()
--- database.py
+++ database.py
@@ -2,7 +2,7 @@ import sqlite3
 import pandas as pd
 
 # Create a table.
-def create_table() :
+def create_table_customers() :
     # Connect to database.
     conn = sqlite3.connect('finances.db')
     # Create a cursor.
@@ -10,70 +10,243 @@ def create_table() :
 
     # Create a table.
     cursor.execute("""
-        CREATE TABLE customers (
+        CREATE TABLE IF NOT EXISTS customers (
             first_name text,
             last_name text,
             email_address text)
         """)
 
-    # Commit our command.
+    # Commit our command and close our connection.
     conn.commit()
+    conn.close()
+
+# Query the database and return all records ordered by specified column.
+def show_records_all_customers(column_information) :
+    # Connect to database.
+    conn = sqlite3.connect('finances.db')
+    # Create a cursor.
+    cursor = conn.cursor()
+        
+    # Query the database.
+    if (column_information == 'First Name') :
+        cursor.execute(""" SELECT *, rowid FROM customers ORDER BY first_name""")
+    elif (column_information == 'Last Name') :
+        cursor.execute(""" SELECT *, rowid FROM customers ORDER BY last_name""")
+    elif (column_information == 'Email Address') :
+        cursor.execute(""" SELECT *, rowid FROM customers ORDER BY email_address""")
+    else :
+        cursor.execute(""" SELECT *, rowid FROM customers ORDER BY rowid""")
+
+    items = cursor.fetchall()
+
+    name_column = []
+    email_address_column = []
+    key_column = []
+
+    for item in range(len(items)) :
+        name_column.append(items[item][0] + ' ' + items[item][1])
+        email_address_column.append(items[item][2]) 
+        key_column.append(str(items[item][3]))
+
+        ## Print item appended to column lists to screen. - > Mainly for debugging purposes.
+        ## print ('*Item Added*')
+        ## print('Name Column Item: ' + str(name_column[item]))
+        ## print('Email_Address Column Item: ' + str(email_address_column[item]))
+        ## print('Key ID Item: ' + str(key_column[item]) + '\n')
+
+    ## Print all column lists to screen. - > Mainly for debugging purposes.
+    ## print ('*Lists Complete*')
+    ## print('Name Column: ' + str(name_column) + '\n')
+    ## print('Email_Address Column: ' + str(email_address_column) + '\n')
+    ## print('Key ID: ' + str(key_column) + '\n\n')
+
+    # Convert the list to a series for each column.
+    name_series = pd.DataFrame(name_column)
+    email_address_series = pd.DataFrame(email_address_column)
+    key_series = pd.DataFrame(key_column)
+
+    ## Print all series to screen. - > Mainly for debugging purposes.
+    ## print ('*Series Converted*')
+    ## print('Name Series: ', str(name_series) + '\n')
+    ## print('Email_Address Column: ' + str(email_address_series) + '\n')
+    ## print('Key ID: ' +str(key_series) + '\n\n')
+
+    # Combine the series into a new dataframe.
+    combined_dataframe = pd.concat([name_series, email_address_series, key_series], axis = 1)
+    combined_dataframe.columns = ['NAME', 'EMAIL', 'PRIMARY KEY']
+
+    ## Print records to screen. - > Mainly for debugging purposes.
+    ## print('All Records :')
+    ## print(combined_dataframe + '\n')
 
     # Close our connection.
     conn.close()
 
+    return combined_dataframe
 
-# Query the database and return all records
-def show_records_all() :
+# Query the database and return a specific record.
+def show_record_specific_customer(key) :
     # Connect to database.
     conn = sqlite3.connect('finances.db')
     # Create a cursor.
     cursor = conn.cursor()
 
     # Query the database.
-    cursor.execute(""" SELECT rowid, * FROM customers """)
-    items = cursor.fetchall()
+    cursor.execute(""" SELECT * FROM customers WHERE rowid = (?) """, str(key))
+    customer_found = cursor.fetchall() 
 
-    key_column = []
-    name_column = []
-    email_address_column = []
+    ## Print record to screen. - > Mainly for debugging purposes.
+    ## print(customer_found)
 
-    for item in items :
-        key_column.append(str(item[0]))
-        name_column.append(item[1] + ' ' + item[2])
-        email_address_column.append(item[3]) 
-
-    # Convert the list to a series for each column.
-    key_series = pd.DataFrame(key_column)
-    name_series = pd.DataFrame(name_column)
-    email_address_series = pd.DataFrame(email_address_column)
-
-    # Combine the series into a new dataframe.
-    combined_dataframe = pd.concat([key_series, name_series, email_address_series], axis = 1)
-    combined_dataframe.columns = ['PRIMARY KEY', 'NAME', 'EMAIL']
-
-    # Print records to screen.
-    print(combined_dataframe)
-
-    # Close our connection.
+    # Commit our command and close our connection.
+    conn.commit()
     conn.close()
 
+    return customer_found[0]
+
 # Add new records to the table.
-def add_record(first_name, last_name, email_address) :
+def add_records_customers(new_first_name, new_last_name, new_email_address) :
+    ## Print parameters passed into function. - > Mainly for debugging purposes.]
+    ## print('New Customer To Be Added :')
+    ## print('First Name : ' + str(first_name_new))
+    ## print('Last Name : ' + str(last_name_new))
+    ## print('Email Address : ' + str(email_address_new) + '\n')
+
     # Connect to database.
     conn = sqlite3.connect('finances.db')
     # Create a cursor.
     cursor = conn.cursor()
 
-    new_customers = [ (first_name[i], last_name[i], email_address[i]) for i in range(len(first_name))] 
+    try :
+        # Insert a single record into database.
+        cursor.execute(""" INSERT INTO customers VALUES (?, ?, ?) """, 
+            (new_first_name, new_last_name, new_email_address)
+        )
 
-    # Insert records into database.
-    cursor.executemany(""" INSERT INTO customers VALUES (?, ?, ?) """, new_customers)
+        ## Print record added to screen. - > Mainly for debugging purposes.
+        ## print('New Customer Added :')
+        ## print('First Name : ' + first_name_new)
+        ## print('Last Name : ' + last_name_new)
+        ## print('Email Address : ' + email_address_new + '\n')
 
-    # Commit our command.
+        # Commit our command.
+        conn.commit()
+    except Exception as manyCustomers :
+        new_customers = []
+
+        for i in range(len(new_first_name)) :
+            new_customers.append((new_first_name[i], new_last_name[i], new_email_address[i]))
+        ## Print record just added to screen. - > Mainly for debugging purposes.
+        ## print('New Customer # ' + str(new_customer + 1) :') 
+        ## print('First Name : ' + str(new_customers[new_customer][0])')
+        ## print('Last Name : ' + str(new_customers[new_customer][1])')
+        ## print('Email Address : ' + str(new_customers[new_customer][2]) + '\n')
+
+        ## Print records to be added to screen. - > Mainly for debugging purposes.
+        ## print('All New Customers :')
+        ## print(str(new_customers) + '\n')
+
+        # Insert many records into database.
+        cursor.executemany(""" INSERT INTO customers VALUES (?, ?, ?) """, 
+            (new_customers)
+        ) 
+
+        ## Print records added to screen. - > Mainly for debugging purposes.
+        ## print(str(len(new_customers)) + 'Customers Added :')
+        ## print(new_customers + '\n')
+
+        # Commit our command.
+        conn.commit()  
+    finally :
+        # Close our connection.
+        conn.close()
+
+# Change the information regarding a specific customer.
+def change_record_specific_customer(key, first_name_new, last_name_new, email_address_new) :
+    # Connect to database.
+    conn = sqlite3.connect('finances.db')
+    # Create a cursor.
+    cursor = conn.cursor()
+
+    # Query the database.
+    cursor.execute(""" UPDATE customers SET
+            first_name = :first_name,
+            last_name = :last_name,
+            email_address = :email_address
+            WHERE rowid = :key """, 
+            
+            {
+                'first_name' : first_name_new,
+                'last_name' : last_name_new,
+                'email_address' : email_address_new,
+                'key' : str(key)
+            }
+        )
+
+    # Commit our command and close our connection.
     conn.commit()
-
-    # Close our connection.
     conn.close()
 
+# Delete a record from the table.
+def delete_record_customer(key) :
+    # Connect to database.
+    conn = sqlite3.connect('finances.db')
+    # Create a cursor.
+    cursor = conn.cursor()
 
+    cursor.execute(""" DELETE from customers WHERE rowid = (?) """, str(key))
+
+    # Commit our command and close our connection.
+    conn.commit()
+    conn.close()
+
+def test_database() :
+    # Create the table.
+    create_table_customers()
+    
+    # Add a customer to the table.
+    add_records_customers('Muntakim', 'Rahman', 'muntakim.rahman@gmail.com')
+
+    print('**Added 1 Customer**')
+    print(str(show_records_all_customers('First Name')) + '\n')
+    muntakim_information = show_record_specific_customer(1)
+    print('First Name : ', muntakim_information[0])
+    print('Last Name : ', muntakim_information[1])
+    print('Email Address : ', muntakim_information[2] + '\n')
+
+    # Add three customers to the table.
+    add_records_customers(
+        ['Mahir', 'Jim', 'Tunzilur', 'Merina'], 
+        ['Tanzil', 'Bob', 'Rahman', 'Sultana'],
+        ['mahirtanzil@gmail.com', 'jimmy@gmail.com', 'tunzilur@gmail.com', 'merina_70@yahoo.com']
+    )
+
+    print('**Added 3 Customers**')
+    print('Current Customers in Table (Arranged by Key ID) :')
+    print(str(show_records_all_customers('Gibberish')) + '\n')
+
+    print('Mahir Current Data :')
+    mahir_information = show_record_specific_customer(2)
+    print('First Name : ', mahir_information[0])
+    print('Last Name : ', mahir_information[1])
+    print('Email Address : ', mahir_information[2] + '\n')
+
+    # Modify Mahir's data in table.
+    change_record_specific_customer(2, 'Mahir Tanzil', 'Rahman', 'mahir199.tanzil@gmail.com')
+    
+    print('**Changed Mahir Name and Email Address**')
+    print('Mahir New Data :')
+    mahir_first_name, mahir_last_name, mahir_email_address = (show_record_specific_customer(2))
+    print('First Name : ', mahir_first_name)
+    print('Last Name : ', mahir_last_name)
+    print('Email Address : ', mahir_email_address + '\n')
+
+    # Delete Jim Bob from the table.
+    delete_record_customer(3)
+
+    print('**Removed Jim Bob**')
+    print('Current Customers in Table (Arranged by Email) :')
+    print(str(show_records_all_customers('Email Address')) + '\n')
+
+## Test the functionality of the Python script.
+## test_database()
